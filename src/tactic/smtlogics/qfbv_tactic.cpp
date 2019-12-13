8eae3f657610e1ebc54c1fd8a3487d63e4225e81
rainoftime <rainoftime@gmail.com>
2019-12-13 06:04:57 +0000

    refine qfbv for klee

--- src/tactic/smtlogics/qfbv_tactic.cpp
+++ src/tactic/smtlogics/qfbv_tactic.cpp
@@ -30,8 +30,43 @@ Notes:
 #include "sat/tactic/sat_tactic.h"
 #include "sat/sat_solver/inc_sat_solver.h"
 #include "ackermannization/ackermannize_bv_tactic.h"
-
+#include <iostream>
 #define MEMLIMIT 300
+
+static tactic * mk_klee_qfbv_preamble(ast_manager& m, params_ref const& p) {
+
+    params_ref solve_eq_p;
+    // conservative guassian elimination.
+    solve_eq_p.set_uint("solve_eqs_max_occs", 2);
+
+    params_ref simp2_p = p;
+    simp2_p.set_bool("som", true);
+    simp2_p.set_bool("pull_cheap_ite", true);
+    simp2_p.set_bool("push_ite_bv", false);
+    simp2_p.set_bool("local_ctx", true);
+    simp2_p.set_uint("local_ctx_limit", 10000000);
+    simp2_p.set_bool("flat", true); // required by som
+    simp2_p.set_bool("hoist_mul", false); // required by som
+
+    //params_ref hoist_p;
+    //hoist_p.set_bool("hoist_mul", true);
+    //hoist_p.set_bool("som", false);
+
+    return
+            and_then(
+                mk_simplify_tactic(m),
+                mk_propagate_values_tactic(m),
+                using_params(mk_solve_eqs_tactic(m), solve_eq_p),
+                mk_elim_uncnstr_tactic(m),
+                //if_no_proofs(if_no_unsat_cores(mk_bv_size_reduction_tactic(m))),
+                //using_params(mk_solve_eqs_tactic(m), solve_eq_p),  // pinpoint: this can handle some "fast sat" cases
+                using_params(mk_simplify_tactic(m), simp2_p),
+                //using_params(mk_simplify_tactic(m), hoist_p),
+                mk_max_bv_sharing_tactic(m)
+                );
+}
+
+
 
 static tactic * mk_qfbv_preamble(ast_manager& m, params_ref const& p) {
 
@@ -65,7 +100,7 @@ static tactic * mk_qfbv_preamble(ast_manager& m, params_ref const& p) {
             // but the timeout in SMT-COMP is too small.
             // Moreover, it impacted negatively some easy benchmarks.
             // We should decide later, if we keep it or not.
-            //
+            //  
             using_params(mk_simplify_tactic(m), hoist_p),
             mk_max_bv_sharing_tactic(m),
             if_no_proofs(if_no_unsat_cores(mk_ackermannize_bv_tactic(m,p)))
@@ -78,6 +113,17 @@ static tactic * main_p(tactic* t) {
     p.set_bool("push_ite_bv", true);
     p.set_bool("blast_distinct", true);
     return using_params(t, p);
+}
+
+
+static tactic * mk_klee_qfbv_tactic(ast_manager& m, params_ref const & p, tactic* sat, tactic* smt) {
+    tactic* preamble_st = mk_klee_qfbv_preamble(m, p);
+    tactic * st = main_p(and_then(preamble_st, 
+                                    cond(mk_is_qfbv_probe(), 
+                                          and_then(mk_bit_blaster_tactic(m),sat),
+                                          smt)));
+    st->updt_params(p);
+    return st;
 }
 
 
@@ -119,11 +165,19 @@ static tactic * mk_qfbv_tactic(ast_manager& m, params_ref const & p, tactic* sat
 }
 
 
+
+
+
 tactic * mk_qfbv_tactic(ast_manager & m, params_ref const & p) {
+    //std::cout << "The INSTANCE is in QF_BV!!!\n";
     tactic * new_sat = cond(mk_produce_proofs_probe(),
                             and_then(mk_simplify_tactic(m), mk_smt_tactic(m)),
                             mk_psat_tactic(m, p));
+#if 0
 
     return mk_qfbv_tactic(m, p, new_sat, mk_smt_tactic(m, p));
+#else 
+    return mk_klee_qfbv_tactic(m, p, new_sat, mk_smt_tactic(m, p));
 
+#endif
 }
